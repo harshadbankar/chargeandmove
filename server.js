df47e8d6403879e9ea8806a83ada51eeb7aa0c91
harshad bankar <harshad.bankar@klm.com>
2016-12-10 17:47:59 +0100

    database name changed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var MongoClient = require('mongodb').MongoClient,
     Code = require('mongodb').Code;
 
 // default to a 'localhost' configuration:
-var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+var connection_string = '127.0.0.1:27017/moveandcharge';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
